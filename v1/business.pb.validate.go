eadfe2f50ae04002061400ed1bf7ccc5939de553
Surendra Kandel <surendrakandel@Surendras-Laptop.local>
2023-01-18 18:05:49 -0800

    update

--- v1/business.pb.validate.go
+++ v1/business.pb.validate.go
@@ -89,6 +89,122 @@ func (m *Business) validate(all bool) error {
 
 	// no validation rules for NeedsDefaultDeliveryAddressUpdate
 
+	if all {
+		switch v := interface{}(m.GetBillingAddress()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "BillingAddress",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "BillingAddress",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetBillingAddress()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return BusinessValidationError{
+				field:  "BillingAddress",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if all {
+		switch v := interface{}(m.GetDefaultPickupAddress()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "DefaultPickupAddress",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "DefaultPickupAddress",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetDefaultPickupAddress()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return BusinessValidationError{
+				field:  "DefaultPickupAddress",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if all {
+		switch v := interface{}(m.GetDefaultDeliveryAddress()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "DefaultDeliveryAddress",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "DefaultDeliveryAddress",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetDefaultDeliveryAddress()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return BusinessValidationError{
+				field:  "DefaultDeliveryAddress",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	if all {
+		switch v := interface{}(m.GetAddress()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "Address",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "Address",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetAddress()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return BusinessValidationError{
+				field:  "Address",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return BusinessMultiError(errors)
 	}

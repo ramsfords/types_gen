59a1e92d1ad8205ad95474c808b3fd1828f187ca
System Administrator <root@Surendras-Laptop.local>
2023-01-26 15:28:13 -0800

    update

--- v1/business.pb.validate.go
+++ v1/business.pb.validate.go
@@ -561,40 +561,6 @@ func (m *HomePageResponse) validate(all bool) error {
 
 	}
 
-	for idx, item := range m.GetQuotes() {
-		_, _ = idx, item
-
-		if all {
-			switch v := interface{}(item).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, HomePageResponseValidationError{
-						field:  fmt.Sprintf("Quotes[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, HomePageResponseValidationError{
-						field:  fmt.Sprintf("Quotes[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return HomePageResponseValidationError{
-					field:  fmt.Sprintf("Quotes[%v]", idx),
-					reason: "embedded message failed validation",
-					cause:  err,
-				}
-			}
-		}
-
-	}
-
 	if len(errors) > 0 {
 		return HomePageResponseMultiError(errors)
 	}
--- v1/quote.pb.validate.go
+++ v1/quote.pb.validate.go
@@ -212,6 +212,35 @@ func (m *QuoteRequest) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetBusiness()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, QuoteRequestValidationError{
+					field:  "Business",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, QuoteRequestValidationError{
+					field:  "Business",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetBusiness()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return QuoteRequestValidationError{
+				field:  "Business",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return QuoteRequestMultiError(errors)
 	}

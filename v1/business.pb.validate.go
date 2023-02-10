5a94a86ddb23a1999a51add903ec1cc87087aaa1
System Administrator <root@Surendras-Laptop.local>
2023-02-09 17:17:21 -0800

    update

--- v1/business.pb.validate.go
+++ v1/business.pb.validate.go
@@ -246,7 +246,34 @@ func (m *Business) validate(all bool) error {
 
 	// no validation rules for BooksOpened
 
-	// no validation rules for AdminUser
+	if all {
+		switch v := interface{}(m.GetAdminUser()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "AdminUser",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, BusinessValidationError{
+					field:  "AdminUser",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetAdminUser()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return BusinessValidationError{
+				field:  "AdminUser",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
 
 	if len(errors) > 0 {
 		return BusinessMultiError(errors)
